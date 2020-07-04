02fab0fb7dc1677fed0460b804ffc36e58d14884
magda sousa de jesus <magda.jesus@hbsis.com.br>
2020-07-04 20:46:58 -0300

    curso da udemy de go

--- pacotes/operadora/nome.go
+++ pacotes/operadora/nome.go
@@ -1,4 +1,19 @@
 package operadora
 
+import (
+	"fmt"
+	"strconv"
+)
+
 //NomeOperadora variavel publica
 var NomeOperadora = "XHXOP Telecon"
+
+//Numero é um código de operadora
+var Numero = 67
+
+//PrefixoDaOperadora testando concatenação
+var PrefixoDaOperadora = strconv.Itoa(Numero) + " " + NomeOperadora
+
+func main() {
+	fmt.Printf("Testando : %s/r/n", PrefixoDaOperadora)
+}
