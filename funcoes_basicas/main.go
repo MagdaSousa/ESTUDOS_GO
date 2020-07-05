6b990cf89102c3f880c7fe4e4f0de144623a25c4
magda sousa de jesus <magda.jesus@hbsis.com.br>
2020-07-04 22:24:52 -0300

    funções em Go

---
+++ funcoes_basicas/main.go
@@ -0,0 +1,19 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+
+// 	resultadoObtido := matematica.divisor(4, 2)
+// 	fmt.Printf(" O resultado da divisão é : %d", resultadoObtido)
+// 	resultadoSoma := matematica.Soma(3, 4)
+// 	fmt.Printf(" O resultado da soma é : %d\n", resultadoSoma)
+// 	resultadoSubtracao := matematica.subtracao(9, 4)
+// 	fmt.Printf(" O resultado da subtração é : %d", resultadoSubtracao)
+// 	resultadoObtido, resto := matematica.divisaoComResto(4, 2)
+// 	fmt.Printf(" O resultado da divisão é : %d resto :%d", resultadoObtido, resto)
+// 	resultadoObtido := matematica.criandoFuncaoMultiplicando(5, 2)
+// 	fmt.Printf(" O resultado da multiplicação foi : %d\n", resultadoObtido)
+// }
---
+++ funcoes_basicas/matematica/multiplicacao.go
@@ -0,0 +1,15 @@
+package matematica
+
+// func main() {
+
+// 	resultadoObtido := criandoFuncaoMultiplicando(5, 2)
+// 	fmt.Printf(" O resultado da multiplicação foi : %d\n", resultadoObtido)
+
+// }
+
+func criandoFuncaoMultiplicando(x int, y int) int {
+	var variavelDeRetorno int
+	variavelDeRetorno = x * y
+
+	return variavelDeRetorno
+}
